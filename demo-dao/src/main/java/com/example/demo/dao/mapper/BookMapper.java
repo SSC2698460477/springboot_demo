002a89704fae74d11c93fcc35dc10c4a3540eb7f
ssc1993 <18303093295@163.com>
2019-08-21 18:11:44 +0800

    新创建BookMapper

---
+++ demo-dao/src/main/java/com/example/demo/dao/mapper/BookMapper.java
@@ -0,0 +1,14 @@
+package com.example.demo.dao.mapper;
+
+import com.example.demo.dao.BaseMapper;
+import com.example.demo.dao.entity.BookInfo;
+
+/**
+ * @program: demo
+ * @description: 操作书籍信息的mapper类
+ * @author: ssc
+ * @create: 2019/8/21 15:46
+ **/
+public interface BookMapper extends BaseMapper<BookInfo> {
+
+}
