d8a96b010de666c09c3176af45d088056cb379fc
ssc1993 <18303093295@163.com>
2019-08-21 18:10:51 +0800

    新创建BookController

---
+++ demo-web/src/main/java/com/example/demo/web/controller/BookController.java
@@ -0,0 +1,67 @@
+package com.example.demo.web.controller;
+
+import com.example.demo.biz.service.BookService;
+import com.example.demo.dao.entity.BookInfo;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @program: demo
+ * @description: book信息操作的controller类
+ * @author: ssc
+ * @create: 2019/8/21 15:21
+ **/
+@RequestMapping("book")
+@RestController
+public class BookController {
+
+    private final static Logger logger = LoggerFactory.getLogger(BookController.class);
+
+    @Autowired
+    private BookService bookService;
+
+    /**
+     * 分页查询书籍信息接口
+     *
+     * @param page 当前页码数
+     * @param rows 每页的条数
+     * @return
+     */
+    @GetMapping("queryBookList")
+    public ResponseEntity<PageInfo> queryBookList(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                                                  @RequestParam(value = "rows", defaultValue = "10") Integer rows) {
+        logger.info("开始调用queryBookList接口，page=" + page + " rows=" + rows);
+        try {
+            PageInfo<BookInfo> pageInfo = bookService.queryPageListByWhere(null, page, rows);
+            return ResponseEntity.ok(pageInfo);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+
+    /**
+     * 根据id 获取书籍的信息
+     *
+     * @param id 书籍id
+     * @return
+     */
+    @GetMapping("queryBookInfoById/{id}")
+    public ResponseEntity<BookInfo> queryBookInfoById(@PathVariable(value = "id") Integer id) {
+
+        logger.info("开始调用queryBookInfoById接口，id=" + id);
+        try {
+            BookInfo bookInfo = bookService.queryById(id);
+            return ResponseEntity.ok(bookInfo);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+
+}
--- demo-web/src/main/java/com/example/demo/web/controller/DemoController.java
+++ demo-web/src/main/java/com/example/demo/web/controller/DemoController.java
@@ -1,17 +1,12 @@
 package com.example.demo.web.controller;
 
 import com.example.demo.biz.service.DemoService;
-import com.example.demo.dao.entity.BookInfo;
-import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @program: demo
@@ -25,25 +20,12 @@ public class DemoController {
 
     private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
 
-    @Resource
+    @Autowired
     private DemoService demoService;
 
     @GetMapping("test")
-    public String test(){
+    public String test() {
         return demoService.test();
     }
 
-    // 根据id 查询书籍信息
-    @GetMapping("queryBookById/{id}")
-    public BookInfo queryBookById(@PathVariable("id") Integer id){
-        return demoService.queryBookById(id);
-    }
-
-    // 分页查询book列表信息
-    @GetMapping("queryBookList")
-    public List<BookInfo> queryBookList(){
-        logger.info("开始调用queryBookList接口");
-        PageInfo<BookInfo> pageInfo = demoService.queryPageListByWhere(null,1,20);
-        return pageInfo.getList();
-    }
 }
